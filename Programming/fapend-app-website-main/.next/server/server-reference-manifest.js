7e2c1e0b9b33c748b48a3c52eb83aa0c5d27caaf
attilabuild <tutorialforhungary@gmail.com>
2025-10-08 15:56:37 +0200

    x

--- Programming/fapend-app-website-main/.next/server/server-reference-manifest.js
+++ Programming/fapend-app-website-main/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"2qi7h6xvCwyqr7kf1JlY7ojwcKBKYP0/+DA+NRIAAUY=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
